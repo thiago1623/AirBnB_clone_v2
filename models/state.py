4ba8c1a87b1e18366bdc147f618f686c5955ea50
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-12-17 19:28:27 +0000

    [update]: refacture

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete, delete-orphan")
+
+    @property
+    def cities(self):
+        """
+        returns the list of City instances with state_id equals
+        to the current State.id
+        """
+        list_city = []
+        for city in models.storage.all(City).values():
+            if city.state_id == self.id:
+                list_city.append(city)
+        return list_city
