faa1fefc8a6e24c7db57321f2daa06ee5618da56
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-12-17 23:48:34 +0000

    [update]: valide edge cases

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state",
-                          cascade="delete")
+                          cascade="all, delete, delete-orphan")
 # all, delete, delete-orphan")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
--- models/user.py
+++ models/user.py
@@ -14,4 +14,5 @@ class User(BaseModel, Base):
     first_name = Column(String(128))
     last_name = Column(String(128))
 
-    places = relationship("Place", backref="user", cascade="delete")
+    places = relationship("Place", backref="user",
+                          cascade="all, delete, delete-orphan")
