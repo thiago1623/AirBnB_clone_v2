54ba316bf074a92d34e1c0d0deb886bf75cac5ac
thiago1623 <thiago.96t16@gmail.com>
2020-12-22 19:12:06 -0500

    [UPDATE]--FILE--(add new changes)

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# script thath sets up the web server for deployment
+
+# install nginx
+sudo apt-get update
+sudo apt-get -y install nginx
+
+#create folders
+sudo mkdir -p /data
+sudo mkdir -p /data/web_static
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/shared/test
+
+# Create a fake HTML file
+echo "Holberton School" >> /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/
+sudo chown -R ubuntu:ubuntu /data
+
+#routing to web static
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+
+#restart nginx
+sudo service nginx restart
