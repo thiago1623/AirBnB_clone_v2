7ceff3ace30102325d6dc1542b8d75d634ed1093
thiago1623 <thiago.96t16@gmail.com>
2020-12-22 22:33:54 -0500

    [UPDATE]--FILE--(add new changes)

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,28 +1,80 @@
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
-import os
+"""
+script that distributes archive to webservers
+"""
+import os.path
 from fabric.api import *
+from fabric.operations import run, put, sudo
 
 env.hosts = ['34.75.156.173', '35.243.219.163']
 
 
-def do_clean(number=0):
-    """Delete out-of-date archives.
+def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = time.strftime("%Y%m%d%H%M%S")
+
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(dt))
+        return ("versions/web_static_{}.tgz".format(dt))
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to a web server.
     Args:
-        number (int): The number of archives to keep.
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    number = 1 if int(number) == 0 else int(number)
+    if os.path.isfile(archive_path) is False:
+        return False
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+    try:
+        file = archive_path.split('/')[-1]
+        new_folder = ("/data/web_static/releases/" + file.split(".")[0])
+        # Upload the archive to the /tmp/
+        put(archive_path, "/tmp/")
+        # Uncompress the archive to the folder /data/web_static/releases/
+        run("sudo mkdir -p {}".format(new_folder))
+        run("sudo tar -xzf /tmp/{} -C {}".format(file, new_folder))
+        # Delete the archive from the web server
+        run("sudo rm /tmp/{}".format(file))
+        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("sudo rm -rf {}/web_static".format(new_folder))
+        # Delete the symbolic link
+        run('sudo rm -rf /data/web_static/current')
+        # Create a new the symbolic link
+        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        return True
+    except:
+        return False
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+
+def deploy():
+    """creates and distributes an archive to your web servers"""
+    try:
+        archive_address = do_pack()
+        val = do_deploy(archive_address)
+        return val
+    except:
+        return False
+
+
+def do_clean(number=0):
+    if number == 0 or number == 1:
+        with cd.local('./versions/'):
+            local("ls -lv | rev | cut -f 1 | rev | \
+            head -n +1 | xargs -d '\n' rm -rf")
+        with cd('/data/web_static/releases/'):
+            run("sudo ls -lv | rev | cut -f 1 | \
+            rev | head -n +1 | xargs -d '\n' rm -rf")
+    else:
+        with cd.local('./versions/'):
+            local("ls -lv | rev | cut -f 1 | rev | \
+            head -n +{} | xargs -d '\n' rm -rf".format(number))
+        with cd('/data/web_static/releases/'):
+            run("sudo ls -lv | rev | cut -f 1 | \
+            rev | head -n +{} | xargs -d '\n' rm -rf".format(number))
