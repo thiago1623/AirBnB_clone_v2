1e83f1f89b0b4d5410c72a45751551d29b514377
thiago1623 <thiago.96t16@gmail.com>
2021-01-20 18:42:28 -0500

    [UPDATE]--FILES--(add new changes)

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Displays an HTML page with a list of all State objects in DBStorage."""
-    states = storage.all("State")
-    return render_template("7-states_list.html", states_id=states)
+    return render_template('7-states_list.html',
+                           states=storage.all("State"))
 
 
 @app.teardown_appcontext
